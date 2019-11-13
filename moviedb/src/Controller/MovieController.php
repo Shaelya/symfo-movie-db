942580be9fd88f22d42efce5de6df87936a7ef75
Shaelya <chadenatm@gmail.com>
2019-11-13 19:34:48 +0100

    Affichage details d'un film

--- moviedb/src/Controller/MovieController.php
+++ moviedb/src/Controller/MovieController.php
@@ -2,18 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
     /**
-     * @Route("/movie", name="movie")
+     * @Route("/", name="movie_list")
      */
     public function index()
     {
+
+        $movies = $this->getDoctrine()->getRepository(Movie::class)->findAll();
+
         return $this->render('movie/index.html.twig', [
-            'controller_name' => 'MovieController',
+            'movies' => $movies,
+        ]);
+    }
+
+    /**
+     * @Route("/movies/{movie}/details", name="movie_single_details")
+     */
+    public function singleMovie(Movie $movie)
+    {
+
+        return $this->render('movie/single.html.twig', [
+            'movie' => $movie,
         ]);
     }
 }
