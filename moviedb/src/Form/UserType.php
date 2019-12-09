d254aec168f68f7942f41ded6e87ba1909e5a943
Shaelya <chadenatm@gmail.com>
2019-12-09 18:39:45 +0100

    contraintes email + mot de passe

--- moviedb/src/Form/UserType.php
+++ moviedb/src/Form/UserType.php
@@ -4,12 +4,13 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 
@@ -18,7 +19,9 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
+            ->add('email', EmailType::class, [
+                'constraints' => [new Assert\Email()]
+            ])
             // ->add('roles')
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
@@ -27,8 +30,9 @@ class UserType extends AbstractType
                 'required' => true,
                 'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Retapez-le'],
+                'constraints' => [new Assert\NotBlank(['normalizer' => 'trim'])]
             ])
-            ->add('Inscription', SubmitType::class)
+            ->add('inscription', SubmitType::class)
         ;
     }
 
