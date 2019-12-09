03f1645deeb57b6038066365f41115f0e9ae71a1
Shaelya <chadenatm@gmail.com>
2019-12-09 18:00:16 +0100

    Formulaire d'inscription

--- moviedb/src/Controller/SecurityController.php
+++ moviedb/src/Controller/SecurityController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 class SecurityController extends AbstractController
 {
@@ -32,5 +36,42 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        // on va traiter l'inscription d'un nouvel utilisateur
+        // création d'un formulaire d'inscription
+        $form = $this->createForm(UserType::class);
+
+        // handleRequest permet de relier les informations de la requête avec le formulaire
+        $form->handleRequest($request);
+
+        // Si le formulaire a bien été envoyé et est valide
+        if($form->isSubmitted() && $form->isValid()) {
+
+            // Récupération des données du formulaire
+            $user = $form->getData();
+            //Ajout du rôle ROLE_USER a l'utilisateur par défaut
+            $user->setRoles(['ROLE_USER']);
+
+            // On doit encoder le mot de passe avant d'enregistrer l'utilisateur
+            $plainPassword = $user->getPassword();
+            $encodedPassword = $encoder->encodePassword($user, $plainPassword);
+            $user->setPassword($encodedPassword);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            // Redirection vers la page de connexion
+            return $this->redirectToRoute('app_login');
+        }
+
+        
+        return $this->render('security/register.html.twig', ['registerForm' => $form->createView()]);
     }
 }
